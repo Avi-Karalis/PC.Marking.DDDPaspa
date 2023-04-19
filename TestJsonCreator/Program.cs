1df5aad499f32342d684cb2f92f664aa349102bb
Andreas Gkizis <75754279+AndreasGkizis@users.noreply.github.com>
2023-04-19 10:00:08 +0300

    -Cleaned up program.cs -Results json starts with an object and not a list

--- TestJsonCreator/Program.cs
+++ TestJsonCreator/Program.cs
@@ -13,38 +13,19 @@ namespace TestJsonCreator
         {
             Randomizer.Seed = new Random(8675309);
 
-            //var ScenarioTypes = new List<ScenarioType>();
-
-            //foreach (var value in Enum.GetValues(typeof(ScenarioType)))
-            //{
-            //    ScenarioTypes.Add(new ScenarioType { ScenarioType = (ScenarioType)value });
-            //}
-
             var numFaker = new Faker();
 
             // case multiple option 1 correct per question
-            var localId = 1;
-            //var examFaker = new Faker<Exam>()
-            //    .RuleFor(x => x.Id, f => localId++)
-            //    .RuleFor(x => x.MarkingState, f => MarkingState.UnMarked)
-            //    .RuleFor(x => x.MaximumScore, f => 0)
-            //    .RuleFor(x => x.OverallExamScore, f => null);
-            //.RuleFor(x => x.Sections, f => null);
 
-
-
-            //var optionIds = 1;
+            // creates options for questions
             var optionFaker = new Faker<Option>()
                 .RuleFor(x => x.MarkValue, f => null)
                 .RuleFor(x => x.Text, f => f.Random.Words(4));
-            //.RuleFor(x=> x.IsCorrect, f=> f.Random.Bool())
-            //.RuleFor(x=> x.Selected, f=> f.Random.Bool())
 
-
+            //creates options and adds a selected and correct option
             var questionFaker = new Faker<Question>()
                 .RuleFor(x => x.Body, f => $"{f.Random.Words(15)} ???")
                 .RuleFor(x => x.QuestionType, f => QuestionType.MCQOneCorrect)
-                //.RuleFor(x=>x.ScenarioType, f=> f.ScenarioType[Random.Number(1,3)])
                 .RuleFor(x => x.ScenarioType, f => f.PickRandom<ScenarioType>())
                 .RuleFor(x => x.MaximumMarks, f => f.Random.Number(1, 5))
                 .RuleFor(x => x.AwardedMarks, f => null)
@@ -53,10 +34,9 @@ namespace TestJsonCreator
                         var options = optionFaker.Generate(4);
                         options[1].IsCorrect = true;
                         options[numFaker.Random.Number(0, 3)].Selected = true;
-
                         return options;
                     });
-
+            //creates a Section
             var sectionFaker = new Faker<Section>()
                 .RuleFor(x => x.Description, f => f.Random.Words(10))
                 .RuleFor(x => x.Weight, f => 0.2)
@@ -67,6 +47,7 @@ namespace TestJsonCreator
                 });
 
 
+            // Creates an umarked exam
             var examFaker = new Faker<Exam>()
                 .RuleFor(x => x.MarkingState, f => MarkingState.UnMarked)
                 .RuleFor(x => x.OverallExamScore, f => null)
@@ -76,35 +57,27 @@ namespace TestJsonCreator
                     return sections;
                 });
 
-            var testExam = examFaker.Generate(1);
+
+            var testExam = examFaker.Generate();
 
 
             // Calculate Exam's maximun score
-
-            foreach (Exam exam in testExam) 
+            int maxScore = 0;
+            foreach (Section section in testExam.Sections)
             {
-                int maxScore = 0;
-                foreach(Section section in exam.Sections)
+                foreach (Question question in section.Questions)
                 {
-                    foreach(Question question in section.Questions)
-                    {
-                        maxScore += question.MaximumMarks;
-                    }
+                    maxScore += question.MaximumMarks;
                 }
-                exam.MaximumScore = maxScore;
             }
+            testExam.MaximumScore = maxScore;
+          
 
-        
             //var json = JsonConvert.SerializeObject(testExam, Formatting.Indented);
 
             //Console.WriteLine(  json);
             //File.WriteAllText("MCQ1CorrectPerQ.json", json);
             //Console.ReadLine();
-
-        }
-
-        public void Optionmaker()
-        {
 
         }
     }
